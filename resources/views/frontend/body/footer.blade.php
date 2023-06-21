9c0dc3fef0a587de7ba9a2da5a363ce4c28f10be
bigmotion001 <bigmotion001@gmail.com>
2023-06-21 05:38:06 -0700

    c5 Hotel management system

---
+++ resources/views/frontend/body/footer.blade.php
@@ -0,0 +1,89 @@
+
+</main>
+
+<footer class="revealed">
+    <div class="footer_bg">
+        <div class="gradient_over"></div>
+        <div class="background-image" data-background="url(img/rooms/3.jpg)"></div>
+    </div>
+    <div class="container">
+        <div class="row move_content">
+            <div class="col-lg-4 col-md-12">
+                <h5>Contacts</h5>
+                <ul>
+                    <li>Baker Street 567, Los Angeles 11023<br>California - US<br><br></li>
+                    <li><strong><a href="#0">info@Paradisehotel.com</a></strong></li>
+                    <li><strong><a href="#0">+434 43242232</a></strong></li>
+                </ul>
+                <div class="social">
+                    <ul>
+                        <li><a href="#0"><i class="bi bi-instagram"></i></a></li>
+                        <li><a href="#0"><i class="bi bi-whatsapp"></i></a></li>
+                        <li><a href="#0"><i class="bi bi-facebook"></i></a></li>
+                        <li><a href="#0"><i class="bi bi-twitter"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 ms-lg-auto">
+                <h5>Explore</h5>
+                <div class="footer_links">
+                    <ul>
+                        <li><a href="index-2.html">Home</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="room-list-1.html">Rooms &amp; Suites</a></li>
+                        <li><a href="news-1.html">News &amp; Events</a></li>
+                        <li><a href="contacts.html">Contacts</a></li>
+                        <li><a href="about.html">Terms and Conditions</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div id="newsletter">
+                    <h5>Newsletter</h5>
+                    <div id="message-newsletter"></div>
+                    <form method="post" action="https://ansonika.com/paradise/phpmailer/newsletter_template_email.php" name="newsletter_form" id="newsletter_form">
+                        <div class="form-group">
+                            <input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+                            <button type="submit" id="submit-newsletter"><i class="bi bi-send"></i></button>
+                        </div>
+                    </form>
+                    <p>Receive latest offers and promos without spam. You can cancel anytime.</p>
+                </div>
+            </div>
+        </div>
+        <!--/row-->
+    </div>
+    <!--/container-->
+    <div class="copy">
+        <div class="container">
+            © Paradise - by <a href="#">Ansonika</a>
+        </div>
+    </div>
+</footer>
+<!-- /footer -->
+
+<div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
+    </svg>
+</div>
+<!-- /back to top -->
+
+<!-- COMMON SCRIPTS -->
+<script src="{{ ('frontend/js/common_scripts.js') }}"></script>
+<script src="{{ ('frontend/js/common_functions.js') }}"></script>
+<script src="{{ ('frontend/js/datepicker_inline.js') }}"></script>
+
+
+<script type="text/javascript">
+    $('.jarallax').jarallax({
+      videoLoop: true,
+      videoPlayOnlyVisible: false,
+      videoLazyLoading: false
+    });
+</script>
+
+</body>
+
+
+</html>
---
+++ resources/views/frontend/body/header.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Ansonika">
+    <title>C5</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- GOOGLE WEB FONT-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500&amp;family=Montserrat:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="{{ ('frontend/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ ('frontend/css/style.css') }}" rel="stylesheet">
+	<link href="{{ ('frontend/css/vendors.min.css') }}" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="{{ ('frontend/css/custom.css') }}" rel="stylesheet">
+</head>
+
+<body class="datepicker_mobile_full">
+
+    <div id="preloader">
+        <div data-loader="circle-side"></div>
+    </div><!-- /Page Preload -->
+
+    <div class="layer"></div><!-- Opacity Mask -->
+
+    <header class="reveal_header">
+        <div class="container">
+            <div class="row align-items-center">
+                 <div class="col-6">
+                    <a href="index-2.html" class="logo_normal"><img src="img/logo.png" width="135" height="45" alt=""></a>
+                    <a href="index-2.html" class="logo_sticky"><img src="img/logo_sticky.png" width="135" height="45" alt=""></a>
+                </div>
+                <div class="col-6">
+                    <nav>
+                        <ul>
+                            <li><a href="#booking_section" class="btn_1 btn_scrollto">Book Now</a></li>
+                            <li>
+                               <div class="hamburger_2 open_close_nav_panel">
+                                    <div class="hamburger__box">
+                                        <div class="hamburger__inner"></div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div><!-- /container -->
+    </header><!-- /Header -->
+
+    <div class="nav_panel">
+        <a href="#" class="closebt open_close_nav_panel"><i class="bi bi-x"></i></a>
+        <div class="logo_panel"><img src="img/logo_sticky.png" width="135" height="45" alt=""></div>
+        <div class="sidebar-navigation">
+            <nav>
+                <ul class="level-1">
+                    <li class="parent"><a href="#0">Home</a>
+                         <ul class="level-2">
+                            <li class="back"><a href="#0">Back</a></li>
+                            <li><a href="index-2.html">Home Video Bg</a></li>
+                            <li><a href="index-3.html">Home Carousel</a></li>
+                            <li><a href="index-4.html">Home FlexSlider</a></li>
+                            <li><a href="index-5.html">Home Youtube/Vimeo</a></li>
+                            <li><a href="index-6.html">Home Parallax</a></li>
+                            <li><a href="index-7.html">Home Parallax 2</a></li>
+                        </ul>
+                    </li>
+                    <li class="parent"><a href="#0">Rooms & Suites</a>
+                        <ul class="level-2">
+                            <li class="back"><a href="#0">Back</a></li>
+                            <li><a href="room-list-1.html">Room list 1</a></li>
+                            <li><a href="room-list-2.html">Room list 2</a></li>
+                            <li><a href="room-list-3.html">Room list 3</a></li>
+                            <li><a href="room-details.html">Room details</a></li>
+                            <li><a href="room-details-booking.html">Working Booking Request</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="restaurant.html">Restaurant</a></li>
+                    <li><a href="news-1.html">News & events</a></li>
+                    <li><a href="contacts.html">Contact</a></li>
+                    <li class="parent"><a href="#0">Other Pages</a>
+                        <ul class="level-2">
+                            <li class="back"><a href="#0">Back</a></li>
+                            <li><a href="404.html">Error Page</a></li>
+                            <li><a href="gallery.html">Masonry Gallery</a></li>
+                            <li><a href="menu-of-the-day.html">Menu of the day</a></li>
+                            <li><a href="modal-advertise-1.html">Modal Advertise</a></li>
+                            <li><a href="cookie-bar.html">GDPR Cookie Bar</a></li>
+                            <li><a href="coming-soon.html" target="_blank">Coming Soon</a></li>
+                        </ul>
+                    </li>
+                    <li class="parent"><a href="#0">Menu Versions</a>
+                        <ul class="level-2">
+                            <li class="back"><a href="#0">Back</a></li>
+                            <li><a href="menu-2.html">Menu Version 2 <span class="custom_badge">Hot</span></a></li>
+                            <li><a href="menu-3.html">Menu Version 3</a></li>
+                            <li><a href="menu-4.html">Menu Version 4</a></li>
+                            <li><a href="menu-5.html">Menu Version 5</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="panel_footer">
+                    <div class="phone_element"><a href="tel://423424234"><i class="bi bi-telephone"></i><span><em>Info and bookings</em>+41 934 121 1334</span></a></div>
+                </div>
+                <!-- /panel_footer -->
+            </nav>
+        </div>
+        <!-- /sidebar-navigation -->
+    </div>
+    <!-- /nav_panel -->
+
+    <main>
