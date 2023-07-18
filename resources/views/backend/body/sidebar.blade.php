69375f02be64a58c76aa50c6e7a930725984eb71
bigmotion001 <bigmotion001@gmail.com>
2023-07-17 17:23:33 -0700

    updated

--- resources/views/backend/body/sidebar.blade.php
+++ resources/views/backend/body/sidebar.blade.php
@@ -1,15 +1,10 @@
-
-
-
-
-
 <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
     <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
             <li class="nav-item me-auto"><a class="navbar-brand" href=""><span class="brand-logo">
                     </span>
 
-                    <h2 class="brand-text">Mouau</h2>
+                    <h2 class="brand-text">C5 Exclusive</h2>
                 </a></li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pe-0" data-bs-toggle="collapse"><i
                         class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i><i
@@ -20,39 +15,166 @@
     <div class="shadow-bottom"></div>
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i
-                        data-feather="home"></i><span class="menu-title text-truncate"
-                        data-i18n="Dashboards">Dashboard</span><span
+            <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="home"></i><span
+                        class="menu-title text-truncate" data-i18n="Dashboards">Dashboard</span><span
                         class="badge badge-light-warning rounded-pill ms-auto me-1"></span></a></li>
 
 
-            <li class=" navigation-header"><span data-i18n="Apps &amp; Pages"> Pages</span><i
+            <li class=" navigation-header"><span data-i18n="Apps &amp; Pages"> Hotel</span><i
                     data-feather="more-horizontal"></i></li>
 
 
-    <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i
+            <li class="  nav-item"><a class="d-flex align-items-center" href="#"><i
+                        data-feather="file-text"></i><span class="menu-title text-truncate"
+                        data-i18n="Invoice">Rooms</span></a>
+                <ul class="menu-content">
+                    <li class="#"><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">Add
+                                Room</span></a>
+                    </li>
+
+                    <li class=""><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Preview">All
+                                Rooms</span></a>
+                            </li>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+
+
+            <li class="  nav-item"><a class="d-flex align-items-center" href="#"><i
                         data-feather="book"></i><span class="menu-title text-truncate"
-                        data-i18n="book">Projects</span></a></li>
+                        data-i18n="Invoice">Bookings</span></a>
+                <ul class="menu-content">
+                    <li class="#"><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">Pending Booking</span></a>
+                    </li>
+
+                    <li class=""><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Preview">Active Bookings</span></a>
+                            </li>
+                    <li class=""><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Preview">Canlled Bookings</span></a>
+                            </li>
+                        </li>
+                    </ul>
+                </li>
 
 
 
 
 
-            <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i
-                        data-feather="check-square"></i><span class="menu-title text-truncate"
-                        data-i18n="Todo">Academic Session</span></a></li>
+
+
+            <li class="  nav-item"><a class="d-flex align-items-center" href="#"><i
+                        data-feather="wifi"></i><span class="menu-title text-truncate"
+                        data-i18n="wifi">Amenities</span></a>
+                <ul class="menu-content">
+                    <li class="#"><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">Manage Amenities</span></a>
+                    </li>
+
+
+                        </li>
+                    </ul>
+                </li>
 
 
 
-<li class=" nav-item"><a class="d-flex align-items-center" href="#"><i
-                        data-feather="users"></i><span class="menu-title text-truncate"
-                        data-i18n="users">Admins</span></a></li>
+
+            <li class="  nav-item"><a class="d-flex align-items-center" href="#"><i
+                data-feather="sun"></i><span class="menu-title text-truncate"
+                data-i18n="wifi">Complements</span></a>
+        <ul class="menu-content">
+            <li class="#"><a class="d-flex align-items-center" href="#"><i
+                        data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">Manage Complements</span></a>
+            </li>
+
+
+                </li>
+            </ul>
+        </li>
 
 
 
-                        <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i
-                            data-feather="user"></i><span class="menu-title text-truncate"
-                            data-i18n="user">Profile</span></a></li>
+                <li class=" navigation-header"><span data-i18n="Apps &amp; Pages"> Extra</span><i
+                    data-feather="more-horizontal"></i></li>
+
+
+            <li class="  nav-item"><a class="d-flex align-items-center" href="#"><i
+                        data-feather="home"></i><span class="menu-title text-truncate"
+                        data-i18n="Invoice">Manage Section</span></a>
+                <ul class="menu-content">
+                    <li class="#"><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">Add
+                                Room</span></a>
+                    </li>
+
+                    <li class=""><a class="d-flex align-items-center" href="#"><i
+                                data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Preview">All
+                                Rooms</span></a>
+                            </li>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+
+
+
+
+
+
+
+                <li class="  nav-item"><a class="d-flex align-items-center" href="#"><i
+                    data-feather="settings"></i><span class="menu-title text-truncate"
+                    data-i18n="Invoice">Settings</span></a>
+            <ul class="menu-content">
+                <li class="#"><a class="d-flex align-items-center" href="#"><i
+                            data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">SEO Settings</span></a>
+                </li>
+
+                <li class="#"><a class="d-flex align-items-center" href="#"><i
+                    data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">Site Settings</span></a>
+        </li>
+
+
+                    </li>
+                </ul>
+            </li>
+
+
+
+
+
+            <li class="  nav-item"><a class="d-flex align-items-center" href="#"><i
+                data-feather="users"></i><span class="menu-title text-truncate"
+                data-i18n="Invoice">Staffs</span></a>
+        <ul class="menu-content">
+            <li class="#"><a class="d-flex align-items-center" href="#"><i
+                        data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="List">Manage Staffs</span></a>
+            </li>
+
+
+
+
+                </li>
+            </ul>
+        </li>
+
+
+
+        <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="users"></i><span
+                    class="menu-title text-truncate" data-i18n="users">Staffs</span></a></li>
+
+
+
+        <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="user"></i><span
+                    class="menu-title text-truncate" data-i18n="user">Manage Staffs</span></a></li>
 
 
 
--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -18,14 +18,7 @@
         <!-- users list start -->
         <section class="app-user-list">
             <div class="row">
-
-
-
-
-
-
-
-    <div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
+<div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
       <div class="card text-center">
         <div class="card-body">
             <a href="">
@@ -69,36 +62,7 @@
             </a>
         </div>
       </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <!-- list and filter end -->
+    </div>     <!-- list and filter end -->
         </section>
         <!-- users list ends -->
 
@@ -110,6 +74,64 @@
 
 
 
+
+<div class="content-wrapper container-xxl p-0">
+    <div class="content-header row">
+    </div>
+    <div class="content-body">
+        <!-- users list start -->
+        <section class="app-user-list">
+            <div class="row">
+<div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
+      <div class="card text-center">
+        <div class="card-body">
+            <a href="">
+          <div class="avatar bg-light-info p-50 mb-1">
+            <div class="avatar-content">
+                <i data-feather='book'></i>
+            </div>
+          </div>
+          <h2 class="fw-bolder">#</h2>
+          <p class="card-text">Total Projects</p>
+        </a>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-sm-4 col-lg-2 col-xl-4 col-md-4">
+      <div class="card text-center">
+        <div class="card-body">
+            <a href="">
+          <div class="avatar bg-light-warning p-50 mb-1">
+            <div class="avatar-content">
+                <i data-feather='wifi'></i>
+            </div>
+          </div>
+          <h2 class="fw-bolder">#</h2>
+          <p class="card-text">Project Uploaded This Year</p>
+        </a>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
+      <div class="card text-center">
+        <div class="card-body">
+            <a href="">
+          <div class="avatar bg-light-danger p-50 mb-1">
+            <div class="avatar-content">
+                <i data-feather='tv'></i>
+            </div>
+          </div>
+          <h2 class="fw-bolder">#</h2>
+          <p class="card-text">Total Admin</p>
+            </a>
+        </div>
+      </div>
+    </div>     <!-- list and filter end -->
+        </section>
+        <!-- users list ends -->
+
+    </div>
+</div>
 
 
 
@@ -157,17 +179,13 @@
                 </div>
             </div>
             <!-- /.box-body -->
-          
+
         </div>
 
         <!-- /.box -->
     </div>
 
 </div>
-
-
-
-
 </div>
 </div>
 </div>
