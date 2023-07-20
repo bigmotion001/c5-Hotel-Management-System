00f27f989f50b9edd39f86220837f08346e1a10e
bigmotion001 <bigmotion001@gmail.com>
2023-07-20 16:15:03 -0700

    sweet alert fixed

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -16,16 +16,6 @@
 
 
 
-<script src="{{ asset('backend/app-assets/vendors/js/extensions/sweetalert2.all.min.js') }}"></script>
-
-
-
-<script src="{{ asset('backend/app-assets/js/scripts/extensions/ext-component-sweet-alerts.min.js') }}"></script>
-<!-- END: Page JS-->
-
-
-
-
 <!-- BEGIN: Vendor JS-->
 <script src="{{ asset('backend/app-assets/vendors/js/vendors.min.js') }}"></script>
 <!-- BEGIN Vendor JS-->
@@ -43,92 +33,96 @@
 
 <!-- BEGIN: Page JS-->
 <script src="{{ asset('backend/app-assets/js/scripts/pages/dashboard-ecommerce.min.js') }}"></script>
+<script src="{{ asset('backend/app-assets/js/scripts/extensions/ext-component-toastr.min.js') }}"></script>
 <!-- END: Page JS-->
 
+{{-- <!--INITIALIZE TOASTR-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script> --}}
+
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{ asset('backend/app-assets/vendors/js/extensions/sweetalert2.all.min.js') }}"></script>
+    <script src="{{ asset('backend/app-assets/vendors/js/extensions/polyfill.min.js') }}"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Page JS-->
+    <script src="{{ asset('backend/app-assets/js/scripts/extensions/ext-component-sweet-alerts.min.js') }}"></script>
+    <!-- END: Page JS-->
+
+
+
+
+   <script src="{{ asset('../assets/vendor_components/datatable/datatables.min.js') }}"></script>
+  <script src="{{ asset('backend/js/pages/data-table.js') }}"></script>
+
+
+
+
+
+
+
+
+    <script src="{{ asset('backend/app-assets/js/scripts/forms/form-quill-editor.min.js') }}"></script>
+  <!-- BEGIN: Page Vendor JS-->
+  <script src="{{ asset('backend/app-assets/vendors/js/editors/quill/katex.min.js') }}"></script>
+  <script src="{{ asset('backend/app-assets/vendors/js/editors/quill/highlight.min.js') }}"></script>
+  <script src="{{ asset('backend/app-assets/vendors/js/editors/quill/quill.min.js') }}"></script>
+  <!-- END: Page Vendor JS-->
+
+  <!---datatable--->
+
+
 <script>
-  function confirmation(e) {
-
-    e.preventDefault();
-    var link = e.currentTarget.getAttribute('href');
-
-
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "To Deleted This Data!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      cancelButtonText: 'No',
-      confirmButtonText: 'Yes, Delete!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Deleted!',
-          'Data Has Been Deleted Successfully.',
-          'success'
-        )
-        window.location.href = link
-      }
-    });
-
-  }
 
 
 
-
-  $(window).on('load', function() {
+  $(window).on('load',  function(){
     if (feather) {
-      feather.replace({
-        width: 14,
-        height: 14
-      });
+      feather.replace({ width: 14, height: 14 });
     }
   })
+</script>
+<script>
 
+$(function(){
 
-  $(function() {
-
-    @if(Session::has('success'))
+@if(Session::has('success'))
     Swal.fire({
-      icon: 'success',
-      title: 'Great!',
-      text: '{{ Session::get('
-      success ') }}'
+    icon: 'success',
+    title: 'Great!',
+    text: '{{ Session::get("success") }}'
+})
+@endif
+});
+
+@if(Session::has('error'))
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '{{ Session::get("error") }}'
     })
-    @endif
-  });
+@endif
 
-  @if(Session::has('error'))
-  Swal.fire({
-    icon: 'error',
-    title: 'Oops...',
-    text: '{{ Session::get('
-    error ') }}'
-  })
-  @endif
-
-  @if(Session::has('warning'))
-  Swal.fire({
-    icon: 'warning',
-    title: 'Oops...',
-    text: '{{ Session::get('
-    warning ') }}'
-  })
-  @endif
+@if(Session::has('warning'))
+    Swal.fire({
+        icon: 'warning',
+        title: 'Oops...',
+        text: '{{ Session::get("warning") }}'
+    })
+@endif
 
 
 
 
 
-  //delete
-  $(function() {
-    $(document).on('click', '#delete', function(e) {
-      e.preventDefault();
-      var link = $(this).attr("href");
+        //delete
+        $(function(){
+        $(document).on('click', '#delete', function(e){
+            e.preventDefault();
+            var link = $(this).attr("href");
 
 
-      Swal.fire({
+    Swal.fire({
         title: 'Are you sure?',
         text: "To Deleted This Data!",
         icon: 'warning',
@@ -149,9 +143,43 @@
       });
 
 
-    });
+      });
 
-  });
+        });
+
+
+
+
+      //toastr notification
+      window.addEventListener('alert', event => {
+      toastr[event.detail.type](event.detail.message,
+      event.detail.title ?? ''), toastr.options = {
+         "closeButton": true,
+         "progressBar": true,
+       }
+     });
+
+
+
+     ClassicEditor
+                                .create( document.querySelector( '#editor' ) )
+                                .then( editor => {
+                                        console.log( editor );
+                                } )
+                                .catch( error => {
+                                        console.error( error );
+                                } );
+
+            	$(window).ready(function() {
+	$("form").on("keypress", function (event) {
+		var keyPressed = event.keyCode || event.which;
+		if (keyPressed === 13) {
+			event.preventDefault();
+			return false;
+		}
+	});
+	});
+
 </script>
 </body>
 
