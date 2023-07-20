fdc4c6a9f22443dc3d3c779115faac6dab1706e8
bigmotion001 <bigmotion001@gmail.com>
2023-07-20 11:13:23 -0700

    site

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,14 +3,6 @@
 @section('content')
 
 
-
-
-
-
-
-
-
-
 <div class="content-wrapper container-xxl p-0">
     <div class="content-header row">
     </div>
@@ -18,51 +10,66 @@
         <!-- users list start -->
         <section class="app-user-list">
             <div class="row">
-<div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
-      <div class="card text-center">
-        <div class="card-body">
-            <a href="">
-          <div class="avatar bg-light-info p-50 mb-1">
-            <div class="avatar-content">
-                <i data-feather='book'></i>
-            </div>
-          </div>
-          <h2 class="fw-bolder">#</h2>
-          <p class="card-text">Total Projects</p>
-        </a>
-        </div>
-      </div>
-    </div>
-    <div class="col-6 col-sm-4 col-lg-2 col-xl-4 col-md-4">
-      <div class="card text-center">
-        <div class="card-body">
-            <a href="">
-          <div class="avatar bg-light-warning p-50 mb-1">
-            <div class="avatar-content">
-                <i data-feather='wifi'></i>
-            </div>
-          </div>
-          <h2 class="fw-bolder">#</h2>
-          <p class="card-text">Project Uploaded This Year</p>
-        </a>
-        </div>
-      </div>
-    </div>
-    <div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
-      <div class="card text-center">
-        <div class="card-body">
-            <a href="">
-          <div class="avatar bg-light-danger p-50 mb-1">
-            <div class="avatar-content">
-                <i data-feather='tv'></i>
-            </div>
-          </div>
-          <h2 class="fw-bolder">#</h2>
-          <p class="card-text">Total Admin</p>
-            </a>
-        </div>
-      </div>
-    </div>     <!-- list and filter end -->
+                <div class="col-6 col-sm-3 col-lg-3 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-info p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='book'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Today's Booked Room</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3 col-lg-2 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-warning p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='book'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Today's Available Room</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3 col-lg-3 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-danger p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='book'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Total Booked Room</p>
+                            </a>
+                        </div>
+                    </div>
+                </div> <!-- list and filter end -->
+                <div class="col-6 col-sm-3 col-lg-3 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-danger p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='book'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Total Room</p>
+                            </a>
+                        </div>
+                    </div>
+                </div> <!-- list and filter end -->
         </section>
         <!-- users list ends -->
 
@@ -82,51 +89,66 @@
         <!-- users list start -->
         <section class="app-user-list">
             <div class="row">
-<div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
-      <div class="card text-center">
-        <div class="card-body">
-            <a href="">
-          <div class="avatar bg-light-info p-50 mb-1">
-            <div class="avatar-content">
-                <i data-feather='book'></i>
-            </div>
-          </div>
-          <h2 class="fw-bolder">#</h2>
-          <p class="card-text">Total Projects</p>
-        </a>
-        </div>
-      </div>
-    </div>
-    <div class="col-6 col-sm-4 col-lg-2 col-xl-4 col-md-4">
-      <div class="card text-center">
-        <div class="card-body">
-            <a href="">
-          <div class="avatar bg-light-warning p-50 mb-1">
-            <div class="avatar-content">
-                <i data-feather='wifi'></i>
-            </div>
-          </div>
-          <h2 class="fw-bolder">#</h2>
-          <p class="card-text">Project Uploaded This Year</p>
-        </a>
-        </div>
-      </div>
-    </div>
-    <div class="col-6 col-sm-4 col-lg-3 col-xl-4 col-md-4">
-      <div class="card text-center">
-        <div class="card-body">
-            <a href="">
-          <div class="avatar bg-light-danger p-50 mb-1">
-            <div class="avatar-content">
-                <i data-feather='tv'></i>
-            </div>
-          </div>
-          <h2 class="fw-bolder">#</h2>
-          <p class="card-text">Total Admin</p>
-            </a>
-        </div>
-      </div>
-    </div>     <!-- list and filter end -->
+                <div class="col-6 col-sm-3 col-lg-3 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-info p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='book'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Total Booking Completed</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3 col-lg-2 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-warning p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='wifi'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Total Running Booking</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3 col-lg-3 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-danger p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='tv'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Total Booking Request</p>
+                            </a>
+                        </div>
+                    </div>
+                </div> <!-- list and filter end -->
+                <div class="col-6 col-sm-3 col-lg-3 col-xl-3 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <a href="">
+                                <div class="avatar bg-light-danger p-50 mb-1">
+                                    <div class="avatar-content">
+                                        <i data-feather='tv'></i>
+                                    </div>
+                                </div>
+                                <h2 class="fw-bolder">#</h2>
+                                <p class="card-text">Total Booking Canceled</p>
+                            </a>
+                        </div>
+                    </div>
+                </div> <!-- list and filter end -->
         </section>
         <!-- users list ends -->
 
@@ -156,13 +178,13 @@
 
 
 <div class="row">
-     <br>
+    <br>
     <div class="col-md-12 ">
 
         <div class="card">
 
             <div class="card-header with-border">
-                <h3 class="card-title ">Manage Projects</h3>
+                <h3 class="card-title ">Todays Booked Rooms</h3>
             </div>
 
             <!-- /.box-header -->
@@ -186,6 +208,55 @@
     </div>
 
 </div>
+<hr>
+
+
+
+
+
+
+
+<div class="row">
+    <br>
+    <div class="col-md-12 ">
+
+        <div class="card">
+
+            <div class="card-header with-border">
+                <h3 class="card-title ">Available Room for Booking</h3>
+            </div>
+
+            <!-- /.box-header -->
+            <div class="card-body">
+
+                <div class="table-responsive ">
+
+                    <table id="example1" class="table table-bordered table-striped">
+
+
+
+                    </table>
+
+                </div>
+            </div>
+            <!-- /.box-body -->
+
+        </div>
+
+        <!-- /.box -->
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
 </div>
 </div>
 </div>
@@ -193,7 +264,6 @@
 </div>
 </section>
 <!-- Modal Sizes end -->
-
 
 
 
