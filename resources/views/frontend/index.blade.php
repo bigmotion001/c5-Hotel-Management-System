68ac70ff9c0a6dbe4754321f85a7c231f8545086
Myron <josephonyekachi77@gmail.com>
2023-07-20 22:05:23 -0800

    updated

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -115,85 +115,81 @@
 
 <div class="container margin_120_95">
     <div class="title mb-3">
-        <small data-cue="slideInUp">Luxury experience</small>
+        <small data-cue="slideInUp">C5 Luxury experience</small>
         <h2 data-cue="slideInUp" data-delay="200">Rooms & Suites</h2>
     </div>
     <div class="row justify-content-center add_bottom_90" data-cues="slideInUp" data-delay="300">
-        <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12">
+        @foreach($roomtype as $key => $room)
+        @if($key < 1) <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12">
             <a href="room-details.html" class="box_cat_rooms">
                 <figure>
                     <div class="background-image" data-background="url(frontend/img/rooms/1.jpg)"></div>
                     <div class="info">
-                        <small>From $250/night</small>
-                        <h3>Junior Suite</h3>
+                        <small>From &#8358;{{$room->fare}}/night</small>
+                        <h3>{{$room->name}}</h3>
                         <span>Read more</span>
                     </div>
                 </figure>
             </a>
-        </div>
-        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-            <a href="room-details.html" class="box_cat_rooms">
-                <figure>
-                    <div class="background-image" data-background="url(frontend/img/rooms/2.jpg)"></div>
-                    <div class="info">
-                        <small>From $190/night</small>
-                        <h3>Deluxe Room</h3>
-                        <span>Read more</span>
-                    </div>
-                </figure>
-            </a>
-        </div>
-        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-            <a href="room-details.html" class="box_cat_rooms">
-                <figure>
-                    <div class="background-image" data-background="url(frontend/img/rooms/3.jpg)"></div>
-                    <div class="info">
-                        <small>From $240/night</small>
-                        <h3>Superior Room</h3>
-                        <span>Read more</span>
-                    </div>
-                </figure>
-            </a>
-        </div>
-        <p class="text-end"><a href="room-list-1.html" class="btn_1 outline mt-2">View all Rooms</a></p>
     </div>
-    <!-- /row-->
+    @endif
+    @endforeach
 
-    <div class="title text-center mb-5">
-        <small data-cue="slideInUp">C5 Hotel</small>
-        <h2 data-cue="slideInUp" data-delay="100">Main Facilities</h2>
-    </div>
-    <div class="row mt-4">
-        <div class="col-xl-3 col-md-6">
-            <div class="box_facilities no-border" data-cue="slideInUp">
-                <!-- <i class="customicon-private-parking"></i> -->
-                <h3>Private Parking</h3>
-                <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="box_facilities" data-cue="slideInUp">
-                <!-- <i class="customicon-wifi"></i> -->
-                <h3>High Speed Wifi</h3>
-                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.</p>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="box_facilities" data-cue="slideInUp">
-                <!-- <i class="customicon-cocktail"></i> -->
-                <h3>Bar & Restaurant</h3>
-                <p>Similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="box_facilities" data-cue="slideInUp">
-                <!-- <i class="customicon-swimming-pool"></i> -->
-                <h3>Swimming Pool</h3>
-                <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
-            </div>
+    @foreach($roomtype as $key => $rooms)
+    @if($key > 0 && $key < 3) <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+        <a href="room-details.html" class="box_cat_rooms">
+            <figure>
+                <div class="background-image" data-background="url(frontend/img/rooms/2.jpg)"></div>
+                <div class="info">
+                    <small>From &#8358;{{$rooms->fare}}/night</small>
+                    <h3>{{$rooms->name}}</h3>
+                    <span>Read more</span>
+                </div>
+            </figure>
+        </a>
+</div>
+@endif
+@endforeach
+
+<p class="text-end"><a href="room-list-1.html" class="btn_1 outline mt-2">View all Rooms</a></p>
+</div>
+<!-- /row-->
+
+<div class="title text-center mb-5">
+    <small data-cue="slideInUp">C5 Hotel</small>
+    <h2 data-cue="slideInUp" data-delay="100">Main Facilities</h2>
+</div>
+<div class="row mt-4">
+    <div class="col-xl-3 col-md-6">
+        <div class="box_facilities no-border" data-cue="slideInUp">
+            <!-- <i class="customicon-private-parking"></i> -->
+            <h3>Private Parking</h3>
+            <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
         </div>
     </div>
-    <!-- /Row -->
+    <div class="col-xl-3 col-md-6">
+        <div class="box_facilities" data-cue="slideInUp">
+            <!-- <i class="customicon-wifi"></i> -->
+            <h3>High Speed Wifi</h3>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.</p>
+        </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+        <div class="box_facilities" data-cue="slideInUp">
+            <!-- <i class="customicon-cocktail"></i> -->
+            <h3>Bar & Restaurant</h3>
+            <p>Similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+        </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+        <div class="box_facilities" data-cue="slideInUp">
+            <!-- <i class="customicon-swimming-pool"></i> -->
+            <h3>Swimming Pool</h3>
+            <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
+        </div>
+    </div>
+</div>
+<!-- /Row -->
 </div>
 <!-- /container-->
 
