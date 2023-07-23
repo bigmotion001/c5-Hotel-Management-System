3c4649af582adc0410abedb161c9254a5f3f66b7
Myron <josephonyekachi77@gmail.com>
2023-07-23 15:33:20 -0800

    updated

--- database/migrations/2023_07_23_233058_create_facilities_table.php
+++ database/migrations/2023_07_23_233058_create_facilities_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ffacilities', function (Blueprint $table) {
+        Schema::create('facilities', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
             $table->string('sub_title')->nullable();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ffacilities');
+        Schema::dropIfExists('facilities');
     }
 };
