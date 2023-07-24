73f1547d2b9ec255a61a9da4053ac82cde3e1457
Myron <josephonyekachi77@gmail.com>
2023-07-23 17:26:57 -0800

    updated

---
+++ resources/views/frontend/body/testimony.blade.php
@@ -0,0 +1,57 @@
+<div class="parallax_section_1 jarallax" data-jarallax data-speed="0.2">
+    <img class="jarallax-img kenburns-2" src="frontend/img/hero_home_1.jpg" alt="">
+    <div class="wrapper opacity-mask d-flex align-items-center justify-content-center text-center" data-opacity-mask="rgba(0, 0, 0, 0.5)">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="title white">
+                        <small class="mb-1">Testimonials</small>
+                        <h2>What Clients Says</h2>
+                    </div>
+                    <div class="carousel_testimonials owl-carousel owl-theme nav-dots-orizontal">
+                        <div>
+                            <div class="box_overlay">
+                                <div class="pic">
+                                    <figure><img src="frontend/img/testimonial_1.jpg" alt="" class="img-circle">
+                                    </figure>
+                                    <h4>Roberta<small>12 Oct</small></h4>
+                                </div>
+                                <div class="comment">
+                                    "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
+                                </div>
+                            </div>
+                            <!-- End box_overlay -->
+                        </div>
+                        <div>
+                            <div class="box_overlay">
+                                <div class="pic">
+                                    <figure><img src="frontend/img/testimonial_1.jpg" alt="" class="img-circle">
+                                    </figure>
+                                    <h4>Roberta<small>2 Nov</small></h4>
+                                </div>
+                                <div class="comment">
+                                    "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
+                                </div>
+                            </div>
+                            <!-- End box_overlay -->
+                        </div>
+                        <div>
+                            <div class="box_overlay">
+                                <div class="pic">
+                                    <figure><img src="frontend/img/testimonial_1.jpg" alt="" class="img-circle">
+                                    </figure>
+                                    <h4>Roberta<small>3 Dec</small></h4>
+                                </div>
+                                <div class="comment">
+                                    "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
+                                </div>
+                            </div>
+                            <!-- End box_overlay -->
+                        </div>
+                    </div>
+                    <!-- End carousel_testimonials -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -96,47 +96,9 @@
 </div>
 <!-- /pinned content -->
 
-<div class="container margin_120_95">
-    <div class="title mb-3">
-        <small data-cue="slideInUp">C5 Luxury experience</small>
-        <h2 data-cue="slideInUp" data-delay="200">Rooms & Suites</h2>
-    </div>
-    <div class="row justify-content-center add_bottom_90" data-cues="slideInUp" data-delay="300">
-        @foreach($roomtype as $key => $room)
-        @if($key < 1) <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12">
-            <a href="{{route('room_details', $room->id)}}" class="box_cat_rooms">
-                <figure>
-                    <div class="background-image" data-background="url(../backend/assets/images/{{$room->image1}})"></div>
-                    <div class="info">
-                        <small>From &#8358;{{$room->fare}}/night</small>
-                        <h3>{{$room->name}}</h3>
-                        <span>Read more</span>
-                    </div>
-                </figure>
-            </a>
-    </div>
-    @endif
-    @endforeach
-
-    @foreach($roomtype as $key => $rooms)
-    @if($key > 0 && $key < 3) <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <a href="{{route('room_details', $rooms->id)}}" class="box_cat_rooms">
-            <figure>
-                <div class="background-image" data-background="url(../backend/assets/images/{{$rooms->image1}})"></div>
-                <div class="info">
-                    <small>From &#8358;{{$rooms->fare}}/night</small>
-                    <h3>{{$rooms->name}}</h3>
-                    <span>Read more</span>
-                </div>
-            </figure>
-        </a>
-</div>
-@endif
-@endforeach
-
-<p class="text-end"><a href="{{route('all_rooms')}}" class="btn_1 outline mt-2">View all Rooms</a></p>
-</div>
-<!-- /row-->
+<!--All Rooms-->
+@include('frontend.body.allRooms')
+<!-- /All Rooms-->
 
 <div class="title text-center mb-5">
     <small data-cue="slideInUp">C5 Hotel</small>
@@ -226,64 +188,10 @@
 </div>
 <!-- /bg_white -->
 
-<div class="parallax_section_1 jarallax" data-jarallax data-speed="0.2">
-    <img class="jarallax-img kenburns-2" src="frontend/img/hero_home_1.jpg" alt="">
-    <div class="wrapper opacity-mask d-flex align-items-center justify-content-center text-center" data-opacity-mask="rgba(0, 0, 0, 0.5)">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-8">
-                    <div class="title white">
-                        <small class="mb-1">Testimonials</small>
-                        <h2>What Clients Says</h2>
-                    </div>
-                    <div class="carousel_testimonials owl-carousel owl-theme nav-dots-orizontal">
-                        <div>
-                            <div class="box_overlay">
-                                <div class="pic">
-                                    <figure><img src="frontend/img/testimonial_1.jpg" alt="" class="img-circle">
-                                    </figure>
-                                    <h4>Roberta<small>12 Oct</small></h4>
-                                </div>
-                                <div class="comment">
-                                    "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
-                                </div>
-                            </div>
-                            <!-- End box_overlay -->
-                        </div>
-                        <div>
-                            <div class="box_overlay">
-                                <div class="pic">
-                                    <figure><img src="frontend/img/testimonial_1.jpg" alt="" class="img-circle">
-                                    </figure>
-                                    <h4>Roberta<small>2 Nov</small></h4>
-                                </div>
-                                <div class="comment">
-                                    "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
-                                </div>
-                            </div>
-                            <!-- End box_overlay -->
-                        </div>
-                        <div>
-                            <div class="box_overlay">
-                                <div class="pic">
-                                    <figure><img src="frontend/img/testimonial_1.jpg" alt="" class="img-circle">
-                                    </figure>
-                                    <h4>Roberta<small>3 Dec</small></h4>
-                                </div>
-                                <div class="comment">
-                                    "Mea ad postea meliore fuisset. Timeam repudiare id eum, ex paulo dictas elaboraret sed, mel cu unum nostrud."
-                                </div>
-                            </div>
-                            <!-- End box_overlay -->
-                        </div>
-                    </div>
-                    <!-- End carousel_testimonials -->
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- /parallax_section_1-->
+
+<!--Testimony-->
+@include('frontend.body.testimony')
+<!--Testimony-->
 
 
 <!--/bg_white -->
