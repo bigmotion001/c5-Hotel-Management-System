bd2ef8bec7d6a3b1597cf80c86bd384609d4b65e
Myron <josephonyekachi77@gmail.com>
2023-07-31 20:45:18 -0800

    updated

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -63,12 +63,38 @@
                 <!--/owl-slide-->
             </div>
         </div>
+<<<<<<< HEAD
+    </div>
+</div>
+<!-- /pinned content -->
+
+<!--All Rooms-->
+@include('frontend.body.allRooms')
+<!-- /All Rooms-->
+
+<div class="title text-center mb-5">
+    <small data-cue="slideInUp">C5 Hotel</small>
+    <h2 data-cue="slideInUp" data-delay="100">Main Facilities</h2>
+</div>
+<div class="row mt-4">
+    @foreach($facilities as $key => $facility)
+    @if($key < 4)
+    <div class="col-xl-3 col-md-6">
+        <a href="{{route('facility_details', $facility->id)}}">
+            <div class="box_facilities no-border" data-cue="slideInUp">
+                <!-- <i class="customicon-private-parking"></i> -->
+                <h3>{{$facility->title}}</h3>
+                <p>{!! Str::limit($facility->sub_title, 75, '...') !!}</p>
+            </div> 
+        </a>
+=======
         <div class="mouse_wp">
             <a href="#first_section" class="btn_scrollto">
                 <div class="mouse"></div>
             </a>
         </div>
         <!-- / mouse -->
+>>>>>>> e473414764b039fc0a62290363f8ff1432e68630
     </div>
     <!--/carousel-->
 
@@ -169,27 +195,43 @@
         </div>
         <!-- /row-->
 
-<div class="title text-center mb-5">
-    <small data-cue="slideInUp">C5 Hotel</small>
-    <h2 data-cue="slideInUp" data-delay="100">Main Facilities</h2>
-</div>
-<div class="row mt-4">
-    @foreach($facilities as $key => $facility)
-    @if($key < 4)
-    <div class="col-xl-3 col-md-6">
-        <div class="box_facilities no-border" data-cue="slideInUp">
-            <!-- <i class="customicon-private-parking"></i> -->
-            <h3>{{$facility->title}}</h3>
-            <p>{{$facility->sub_title}}</p>
+        <div class="title text-center mb-5">
+            <small data-cue="slideInUp">C5 Hotel</small>
+            <h2 data-cue="slideInUp" data-delay="100">Main Facilities</h2>
         </div>
+        <div class="row mt-4">
+            <div class="col-xl-3 col-md-6">
+                <div class="box_facilities no-border" data-cue="slideInUp">
+                    <!-- <i class="customicon-private-parking"></i> -->
+                    <h3>Private Parking</h3>
+                    <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="box_facilities" data-cue="slideInUp">
+                    <!-- <i class="customicon-wifi"></i> -->
+                    <h3>High Speed Wifi</h3>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="box_facilities" data-cue="slideInUp">
+                    <!-- <i class="customicon-cocktail"></i> -->
+                    <h3>Bar & Restaurant</h3>
+                    <p>Similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="box_facilities" data-cue="slideInUp">
+                    <!-- <i class="customicon-swimming-pool"></i> -->
+                    <h3>Swimming Pool</h3>
+                    <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
+                </div>
+            </div>
+        </div>
+        <!-- /Row -->
     </div>
-    @endif
-    @endforeach
-    
-</div>
-<!-- /Row -->
-</div>
-<!-- /container-->
+    <!-- /container-->
 
     <div class="marquee">
         <div class="track">
